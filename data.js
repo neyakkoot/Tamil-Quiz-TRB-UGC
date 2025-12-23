88c579ffd8bbe6549e9d02b9571d268617585afb
Dr. Sathiyaraj Thangasamy <78794135+neyakkoot@users.noreply.github.com>
2025-12-23 17:51:23 +0530

    Create data.js
    Create data.js

---
+++ data.js
@@ -0,0 +1,144 @@
+const masterData = [
+  {
+    "category": "தமிழ் முதன்மைத் தாள்_தாள் 2",
+    "quizzes": [
+      { "file": "Tamil_TRB_Test_1.json", "title": "தமிழ் இலக்கிய, இலக்கணம் 1" },
+      { "file": "Tamil_TRB_Test_2.json", "title": "தமிழ் இலக்கிய, இலக்கணம் 2" },
+      { "file": "Tamil_TRB_Test_3.json", "title": "தமிழ் இலக்கிய, இலக்கணம் 3" },
+      { "file": "Tamil_TRB_Test_4.json", "title": "தமிழ் இலக்கிய, இலக்கணம் 4" },
+      { "file": "Tamil_TRB_Test_5.json", "title": "தமிழ் இலக்கிய, இலக்கணம் 5" },
+      { "file": "Tamil_TRB_Test_6.json", "title": "ஆழ்நிலைக்கற்றல் முறை 1" },
+      { "file": "Tamil_TRB_Test_7.json", "title": "ஆழ்நிலைக்கற்றல் முறை 2" },
+      { "file": "Tamil_TRB_Test_8.json", "title": "ஆழ்நிலைக்கற்றல் முறை 3" },
+      { "file": "Tamil_TRB_Test_9.json", "title": "தமிழ் இலக்கண வரலாறு 1" },
+      { "file": "Tamil_TRB_Test_10.json", "title": "தமிழ் இலக்கண வரலாறு 2" },
+      { "file": "Tamil_TRB_Test_11.json", "title": "தமிழ் இலக்கண வரலாறு 3" },
+      { "file": "Tamil_TRB_Test_20.json", "title": "தமிழ் இலக்கண வரலாறு 4" },
+      { "file": "Tamil_TRB_Test_21.json", "title": "தமிழ் இலக்கண வரலாறு 5" },
+      { "file": "Tamil_TRB_Test_22.json", "title": "தமிழ் இலக்கண வரலாறு 6" },
+      { "file": "Tamil_TRB_Test_12.json", "title": "தமிழ் இலக்கிய வரலாறு 1" },
+      { "file": "Tamil_TRB_Test_13.json", "title": "தமிழ் இலக்கிய வரலாறு 2" },
+      { "file": "Tamil_TRB_Test_14.json", "title": "தமிழ் இலக்கிய வரலாறு 3" },
+      { "file": "Tamil_TRB_Test_15.json", "title": "தமிழ் இலக்கிய வரலாறு 4" },
+      { "file": "Tamil_TRB_Test_16.json", "title": "தமிழ் இலக்கிய வரலாறு 5" },
+      { "file": "Tamil_TRB_Test_17.json", "title": "தமிழ் இலக்கிய வரலாறு 6" },
+      { "file": "Tamil_TRB_Test_18.json", "title": "தமிழ் இலக்கிய வரலாறு 7" },
+      { "file": "Tamil_TRB_Test_19.json", "title": "தமிழ் இலக்கிய வரலாறு 8" },
+      { "file": "Tamil_TRB_1.json", "title": "பயிற்சி 1" },
+      { "file": "Tamil_TRB_2.json", "title": "பயிற்சி 2" },
+      { "file": "Tamil_TRB_3.json", "title": "பயிற்சி 3" },
+      { "file": "Tamil_TRB_5.json", "title": "பயிற்சி 5" },
+      { "file": "Tamil_TRB_6.json", "title": "பயிற்சி 6" },
+      { "file": "Tamil_TRB_7.json", "title": "பயிற்சி 7" },
+      { "file": "Tamil_TRB_8.json", "title": "பயிற்சி 8" },
+      { "file": "Tamil_TRB_9.json", "title": "பயிற்சி 9" },
+      { "file": "Tamil_TRB_10.json", "title": "பயிற்சி 10" },
+      { "file": "Tamil_TRB_11.json", "title": "பயிற்சி 11" }
+    ]
+  },
+  {
+    "category": "தமிழ்த் தகுதித் தேர்வு_தாள் 1",
+    "quizzes": [
+      { "file": "TET_Tamil_1.json", "title": "பயிற்சி 1" },
+      { "file": "TET_Tamil_2.json", "title": "பயிற்சி 2" },
+      { "file": "TET_Tamil_3.json", "title": "பயிற்சி 3" },
+      { "file": "TET_Tamil_4.json", "title": "பயிற்சி 4" },
+      { "file": "TET_1.json", "title": "TET பயிற்சி 1" },
+      { "file": "TET_2.json", "title": "TET பயிற்சி 2" },
+      { "file": "TET_3.json", "title": "TET பயிற்சி 3" },
+      { "file": "TET_4.json", "title": "TET பயிற்சி 4" }
+    ]
+  },
+  {
+    "category": "தமிழ்க் களஞ்சியம் (பயிற்சி & ஆழ்நிலை)",
+    "quizzes": [
+      { "file": "Mozhiyiyal.json", "title": "மொழியியல் 1" },
+      { "file": "Nattupuraviyal.json", "title": "நாட்டுப்புறவியல் 1" },
+      { "file": "Ilakkiyaththiranaivu.json", "title": "இலக்கியத் திறனாய்வு 1" },
+      { "file": "Tolkappiyam.json", "title": "தொல்காப்பியம் 1" },
+      { "file": "Ithazhiyal.json", "title": "இதழியல் 1" },
+      { "file": "Mozhipeyarppiyal.json", "title": "மொழிபெயர்ப்பியல் 1" },
+      { "file": "Tholliyal.json", "title": "தொல்லியல் 1" },
+      { "file": "Mozhiyiyal_2.json", "title": "மொழியியல் 2" },
+      { "file": "Nattupuraviyal_2.json", "title": "நாட்டுப்புறவியல் 2" },
+      { "file": "Ilakkiyaththiranaivu_2.json", "title": "இலக்கியத் திறனாய்வு 2" },
+      { "file": "Tolkappiyam_2.json", "title": "தொல்காப்பியம் 2" },
+      { "file": "Ithazhiyal_2.json", "title": "இதழியல் 2" },
+      { "file": "Mozhipeyarppiyal_2.json", "title": "மொழிபெயர்ப்பியல் 2" },
+      { "file": "Tholliyal_2.json", "title": "தொல்லியல் 2" },
+      { "file": "Mozhiyiyal_3.json", "title": "மொழியியல் 3" },
+      { "file": "Nattupuraviyal_3.json", "title": "நாட்டுப்புறவியல் 3" },
+      { "file": "Ilakkiyaththiranaivu_3.json", "title": "இலக்கியத் திறனாய்வு 3" },
+      { "file": "Tolkappiyam_3.json", "title": "தொல்காப்பியம் 3" },
+      { "file": "Ithazhiyal_3.json", "title": "இதழியல் 3" },
+      { "file": "Mozhipeyarppiyal_3.json", "title": "மொழிபெயர்ப்பியல் 3" },
+      { "file": "Tholliyal_3.json", "title": "தொல்லியல் 3" },
+      { "file": "Classical_Literature_1.json", "title": "சங்க இலக்கியம் 1" },
+      { "file": "Classical_Literature_2.json", "title": "சங்க இலக்கியம் 2" },
+      { "file": "Classical_Literature_3.json", "title": "சங்க இலக்கியம் 3" }
+    ]
+  },
+  {
+    "category": "இலக்கண வரலாறு (இளங்குமரன் & சோம. இளவரசு)",
+    "quizzes": [
+      { "file": "TamilGrammar_1.json", "title": "இலக்கண நூல்கள், உரையாசிரியர்கள்" },
+      { "file": "TamilGrammar_2.json", "title": "இலக்கண வரலாறு 1" },
+      { "file": "TamilGrammar_3.json", "title": "முந்து நூல்" },
+      { "file": "TamilGrammar_4.json", "title": "அகத்தியம்" },
+      { "file": "TamilGrammar_5.json", "title": "தொல்காப்பியம் 1" },
+      { "file": "TamilGrammar_6.json", "title": "தொல்காப்பிய உரைகள் 1" },
+      { "file": "TamilGrammar_7.json", "title": "இறையனார் அகப்பொருள்" },
+      { "file": "TamilGrammar_8.json", "title": "புறப்பொருள் வெண்பாமாலை & உரை" },
+      { "file": "TamilGrammar_12.json", "title": "தண்டியலங்காரம்" },
+      { "file": "TamilGrammar_13.json", "title": "நன்னூல்" },
+      { "file": "TamilGrammar_20.json", "title": "இலக்கண வரலாறு 2" },
+      { "file": "TamilGrammar_30.json", "title": "முத்துவீரியம் உள்ளிட்டவை" }
+    ]
+  },
+  {
+    "category": "தமிழ்த் தகவல் களஞ்சியம் (தேவிரா & பிற)",
+    "quizzes": [
+      { "file": "Tamil_Language_Literture-quiz_Mozhi-1.json", "title": "மொழி" },
+      { "file": "Tamil_Language_Literture-quiz_Muchangam-2.json", "title": "முச்சங்கம்" },
+      { "file": "Tamil_Language_Literture-quiz_Sanga Ilakkiyam-3.json", "title": "சங்க இலக்கியங்கள்" },
+      { "file": "Tamil_Language_Literture-quiz_Kaappiyam-5.json", "title": "காப்பியங்கள்" },
+      { "file": "Tamil_Language_Literture-quiz_Folklore-9.json", "title": "நாட்டுபுறவியல்" },
+      { "file": "Tamil_Language_Literture-quiz_Modern Literature-11.json", "title": "இக்கால இலக்கியம்" },
+      { "file": "Tamil_Language_Literture-quiz_Journal-26.json", "title": "இதழ்" },
+      { "file": "Tamil_Language_Literture-quiz_Archeaology-30.json", "title": "கல்வெட்டுத் தமிழ்" }
+    ]
+  },
+  {
+    "category": "சங்க இலக்கியம் & இலக்கணங்கள்",
+    "quizzes": [
+      { "file": "Natrinai.json", "title": "நற்றிணை" },
+      { "file": "Kuruntogai.json", "title": "குறுந்தொகை" },
+      { "file": "Aingurunuru.json", "title": "ஐங்குறுநூறு" },
+      { "file": "Paditruppaththu.json", "title": "பதிற்றுப்பத்து" },
+      { "file": "Agananuru.json", "title": "அகநானூறு" },
+      { "file": "Purananuru.json", "title": "புறநானூறு" },
+      { "file": "Tirukkural.json", "title": "திருக்குறள்" },
+      { "file": "Nannul.json", "title": "நன்னூல்" },
+      { "file": "Tolkappiyam.json", "title": "தொல்காப்பியம்" },
+      { "file": "Purapporul_Venpaa_Malai.json", "title": "புறப்பொருள் வெண்பா மாலை" }
+    ]
+  },
+  {
+    "category": "பள்ளிப் பாடப்புத்தகங்கள் (6 - 10)",
+    "quizzes": [
+      { "file": "6th_Tamil1.json", "title": "ஆறாம் வகுப்பு 1" },
+      { "file": "10th-quiz-questions-1.json", "title": "பத்தாம் வகுப்பு 1" },
+      { "file": "10th_Chapter_1.json", "title": "பத்தாம் வகுப்பு இயல் 1" },
+      { "file": "10th_Chapter_9.json", "title": "பத்தாம் வகுப்பு இயல் 9" }
+    ]
+  },
+  {
+    "category": "தமிழ்த் தகுதித் தேர்வு (TET & TNPSC)",
+    "quizzes": [
+      { "file": "TET_LAW College_TRB_2025.json", "title": "சட்டக் கல்லூரி 2025" },
+      { "file": "TET_PGTRB_2025.json", "title": "TET PGTRB 2025" },
+      { "file": "TNPSC-TETquiz-Unit-1.json", "title": "அலகு I: இலக்கணம்" },
+      { "file": "TNPSC-TETquiz-Unit-7.1.json", "title": "அலகு VII.1: இலக்கியம்" }
+    ]
+  }
+];
